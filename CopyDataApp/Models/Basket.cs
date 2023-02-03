adabb9c07874218caf547f34a736f1088cb47ade
Stanislaw Makievsky <burnfeniks@yandex.ru>
2023-02-03 14:02:14 +0300

    first commit

---
+++ CopyDataApp/Models/Basket.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyDataApp.Models
+{
+    internal class Basket
+    {
+        public int ItemsCount { get; set; }
+
+        public int GetBasketAppleCount() {
+            Basket apple = new Basket();
+            Basket orange = new Basket();
+
+            apple.ItemsCount = 5;
+            orange.ItemsCount += 20;
+
+            return orange.ItemsCount;
+        }
+    }
+}
---
+++ CopyDataApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using CopyDataApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyDataApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program pg = new Program();
+            Basket basket = new Basket();
+            Console.WriteLine("В стеке:");
+            Console.WriteLine(pg.GetAppleCount());
+
+            Console.WriteLine("В куче:");
+            Console.WriteLine(basket.GetBasketAppleCount());
+            Console.ReadKey();
+        }
+
+        public int GetAppleCount() {
+            int apple = 5;
+            int orange = apple;
+            orange += 20;
+            return apple;
+        }
+    }
+}
